9e2cc3ab4b5dfa2c6df084b1845545f9697f6aee
AfraGngr <67650031+AfraGngr@users.noreply.github.com>
2023-10-23 10:51:01 +0300

    refactor: make the test fail

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,7 +5,7 @@ const req = request(app);
 
 it('should return 200', async () => {
     const res = await req.get('/test');
-    expect(res.body.message).toBe('It is a success message');
+    expect(res.body.message).toBe('It a success message');
 });
 
 afterAll(async () => server.close());
